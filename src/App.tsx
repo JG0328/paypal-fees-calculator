1e4425690d57bd2bb35a352680e2e3a496e3ee45
José Gabriel González <jgg032899@gmail.com>
2022-04-11 00:14:49 -0400

    Added final result to page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container, CssBaseline, Divider, Grid, Paper, Stack, TextField, Typography } from "@mui/material";
+import { useEffect, useMemo, useState } from "react";
+import logo from "./paypal-logo.png";
 
-function App() {
+const App = () => {
+  const [amount, setAmount] = useState<number>(0);
+
+  const [result, setResult] = useState<number>(0);
+
+  useEffect(() => {
+    if (amount > 0) {
+      // Applying fees
+      setResult((amount / (1 - 0.054)) + 0.3);
+    }
+  }, [amount]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container component={"main"} maxWidth={"md"}>
+      <CssBaseline />
+      <Stack sx={{ margin: 4 }}>
+        <Paper elevation={3} sx={{ width: "100%", padding: 4 }}>
+          <Grid container spacing={3} direction={"column"}>
+            {/* Top Section */}
+            <Grid container item>
+              <Grid container spacing={1} direction={"column"}>
+                <Grid container item>
+                  <Grid container item spacing={3} alignItems={"center"}>
+                    <Grid item>
+                      <img src={logo} width={48} />
+                    </Grid>
+                    <Grid item>
+                      <Typography variant={"h6"}>
+                        Fees Calculator
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                </Grid>
+                <Grid item>
+                  <Divider />
+                </Grid>
+              </Grid>
+            </Grid>
+            {/* Calculator */}
+            <Grid container item>
+              <Grid container item spacing={3} alignItems={"center"}>
+                <Grid item>
+                  <TextField label={"Transaction Amount ($)"} variant={"filled"} type={"number"} value={amount} onChange={e => setAmount(Number(e.target.value))} />
+                </Grid>
+              </Grid>
+            </Grid>
+            {/* Resut */}
+            <Grid item>
+              <Typography>
+                Result: $ {result.toFixed(2)}
+              </Typography>
+            </Grid>
+          </Grid>
+        </Paper>
+      </Stack>
+    </Container>
+  )
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(
-  <React.StrictMode>
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { createTheme, ThemeProvider } from '@mui/material';
+
+const darkTheme = createTheme({
+  palette: { mode: "dark" }
+})
+
+const container = document.getElementById('root');
+if (!container) {
+  throw new Error("Couldn't find app root");
+}
+const root = createRoot(container);
+root.render(
+  <ThemeProvider theme={darkTheme}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ThemeProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
