d60877ff1a49d4bff36201de66159204f1c2280b
José Gabriel González <jgg032899@gmail.com>
2022-12-04 20:25:27 -0400

    UI improvements
    - Using 3 decimal places instead of 2
    - Adding a navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,96 +1,125 @@
-import { Button, Container, CssBaseline, Divider, Grid, Paper, Stack, TextField, Typography } from "@mui/material";
-import { useEffect, useState } from "react";
-import logo from "./paypal-logo.png";
+import {
+    Button,
+    Container,
+    CssBaseline,
+    Divider, FilledInput, FormControl,
+    Grid,
+    InputAdornment, InputLabel,
+    Paper,
+    Stack,
+    TextField,
+    Typography
+} from "@mui/material";
+import React, {useEffect, useState} from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
+import Navbar from "./Navbar";
 
 const App = () => {
-  const [amount, setAmount] = useState<number>(0);
-  const [fee, setFee] = useState<number>(0);
-  const [result, setResult] = useState<number>(0);
+    const [amount, setAmount] = useState<number>(1.000);
+    const [fee, setFee] = useState<number>(0);
+    const [result, setResult] = useState<number>(0);
 
-  const handleAmountChange = (event: React.ChangeEvent<HTMLInputElement>) => setAmount(Number(event.target.value));
+    const handleAmountChange = (event: React.ChangeEvent<HTMLInputElement>) => setAmount(Number(event.target.value));
 
-  useEffect(() => {
-    if (amount >= 0) {
-      // Applying fees
-      setResult((amount / (1 - 0.054)) + 0.3);
-    }
-  }, [amount]);
+    useEffect(() => {
+        if (amount >= 0) {
+            // Applying fees
+            setResult((amount + 0.3) / (1 - 0.054));
+        }
+    }, [amount]);
 
-  useEffect(() => {
-    if (result >= 0) {
-      // Getting fee
-      setFee(result - amount);
-    }
-  }, [result, amount]);
+    useEffect(() => {
+        if (result >= 0) {
+            // Getting fee
+            setFee(result - amount);
+        }
+    }, [result, amount]);
 
-  return (
-    <Container component={"main"} maxWidth={"md"}>
-      <CssBaseline />
-      <Stack sx={{ margin: 4 }}>
-        <Paper elevation={3} sx={{ width: "100%", padding: 4 }}>
-          <Grid container spacing={3} direction={"column"}>
-            {/* Top Section */}
-            <Grid container item>
-              <Grid container spacing={1} direction={"column"}>
-                <Grid container item>
-                  <Grid container item spacing={3} alignItems={"center"}>
-                    <Grid item>
-                      <img alt={"PayPal Logo"} src={logo} width={48} />
-                    </Grid>
-                    <Grid item>
-                      <Typography variant={"h6"}>
-                        Fees Calculator
-                      </Typography>
-                    </Grid>
-                  </Grid>
-                </Grid>
-                <Grid item>
-                  <Divider />
-                </Grid>
-              </Grid>
-            </Grid>
-            {/* Calculator */}
-            <Grid container item>
-              <Grid container item spacing={3} alignItems={"center"}>
-                <Grid item xs={12} sm>
-                  <TextField label={"Transaction Amount ($)"} variant={"filled"} type={"number"} value={amount.toString()} onChange={handleAmountChange} fullWidth />
-                </Grid>
-                <Grid item xs={12} sm>
-                  <TextField label={"Fee To Apply ($)"} value={fee.toFixed(2)} variant={"filled"} disabled fullWidth />
-                </Grid>
-                <Grid item xs={12} sm>
-                  <TextField label={"Result ($)"} value={result.toFixed(2)} variant={"filled"} disabled fullWidth />
-                </Grid>
-              </Grid>
-            </Grid>
-            {/* Bottom section */}
-            <Grid container item>
-              <Grid container item spacing={1} direction={"column"}>
-                <Grid item>
-                  <Divider />
-                </Grid>
-                <Grid container item>
-                  <Grid container item spacing={3} justifyContent={"flex-end"}>
-                    <Grid item>
-                      <Button
-                        variant={"contained"}
-                        LinkComponent={"a"}
-                        href={"https://github.com/JG0328/paypal-fees-calculator"}
-                        target={"_blank"}
-                      >
-                        <GitHubIcon />
-                      </Button>
-                    </Grid>
-                  </Grid>
-                </Grid>
-              </Grid>
-            </Grid>
-          </Grid>
-        </Paper>
-      </Stack>
-    </Container>
-  )
+    return (
+        <>
+            <Navbar/>
+            <Container disableGutters component={"main"} maxWidth={"sm"} sx={{pt: 8, pb: 6}}>
+                <CssBaseline/>
+                <Stack sx={{margin: 4}}>
+                    <Paper elevation={3} sx={{width: "100%", padding: 4}}>
+                        <Grid container spacing={3} direction={"column"}>
+                            {/* Calculator */}
+                            <Grid item>
+                                <FormControl fullWidth variant={"filled"}>
+                                    <InputLabel htmlFor={"transaction-amount"}>Transaction</InputLabel>
+                                    <FilledInput
+                                        id={"transaction-amount"}
+                                        value={amount.toString()}
+                                        onChange={handleAmountChange}
+                                        startAdornment={<InputAdornment position="start">$</InputAdornment>}
+                                        type={"number"}
+                                        fullWidth
+                                    />
+                                </FormControl>
+                            </Grid>
+                            <Grid item>
+                                <FormControl fullWidth variant={"filled"}>
+                                    <InputLabel htmlFor={"fee-amount"}>Fee</InputLabel>
+                                    <FilledInput
+                                        id={"fee-amount"}
+                                        value={fee.toFixed(3)}
+                                        startAdornment={<InputAdornment position="start">$</InputAdornment>}
+                                        inputProps={{
+                                            readOnly: true
+                                        }}
+                                        type={"number"}
+                                        fullWidth
+                                    />
+                                </FormControl>
+                            </Grid>
+                            <Grid item>
+                                <FormControl fullWidth variant={"filled"}>
+                                    <InputLabel htmlFor={"result-amount"}>Total</InputLabel>
+                                    <FilledInput
+                                        id={"result-amount"}
+                                        value={result.toFixed(3)}
+                                        startAdornment={<InputAdornment position="start">$</InputAdornment>}
+                                        inputProps={{
+                                            readOnly: true
+                                        }}
+                                        type={"number"}
+                                        fullWidth
+                                    />
+                                </FormControl>
+                            </Grid>
+                            {/* Bottom section */}
+                            <Grid item container>
+                                <Grid item container spacing={1} direction={"column"}>
+                                    <Grid item>
+                                        <Divider/>
+                                    </Grid>
+                                    <Grid item container>
+                                        <Grid item container spacing={3} alignItems={"center"} justifyContent={"space-between"}>
+                                            <Grid item>
+                                                <Typography variant={"overline"}>
+                                                    Using <strong>5.4% + 0.30 Fee</strong>
+                                                </Typography>
+                                            </Grid>
+                                            <Grid item>
+                                                <Button
+                                                    variant={"contained"}
+                                                    LinkComponent={"a"}
+                                                    href={"https://github.com/JG0328/paypal-fees-calculator"}
+                                                    target={"_blank"}
+                                                >
+                                                    <GitHubIcon/>
+                                                </Button>
+                                            </Grid>
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Paper>
+                </Stack>
+            </Container>
+        </>
+    )
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,24 @@
 import './index.css';
 import App from './App';
-import { createRoot } from "react-dom/client";
+import {createRoot} from "react-dom/client";
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { createTheme, ThemeProvider } from '@mui/material';
+import {createTheme, ThemeProvider} from '@mui/material';
 
 const darkTheme = createTheme({
-  palette: { mode: "dark" }
+    palette: {mode: "dark"}
 })
 
 const container = document.getElementById('root');
 if (!container) {
-  throw new Error("Couldn't find app root");
+    throw new Error("Couldn't find app root");
 }
 const root = createRoot(container);
 root.render(
-  <ThemeProvider theme={darkTheme}>
-    <App />
-  </ThemeProvider>
+    <ThemeProvider theme={darkTheme}>
+        <App/>
+    </ThemeProvider>
 );
