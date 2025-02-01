b4744ab213a0a15297b4c702f2a77b2da3f99297
José Gabriel González <jgg032899@gmail.com>
2025-02-01 18:39:44 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import {
     Grid2,
     InputAdornment, InputLabel,
     Paper,
-    Stack,
     Typography
 } from "@mui/material";
 import React, { useEffect, useState } from "react";
@@ -40,83 +39,81 @@ const App = () => {
             <Navbar />
             <Container disableGutters component={"main"} maxWidth={"sm"} sx={{ pt: 8, pb: 6 }}>
                 <CssBaseline />
-                <Stack sx={{ margin: 4 }}>
-                    <Paper elevation={3} sx={{ width: "100%", padding: 4 }}>
-                        <Grid2 container spacing={3} direction={"column"}>
-                            {/* Calculator */}
-                            <Grid2>
-                                <FormControl fullWidth variant={"filled"}>
-                                    <InputLabel htmlFor={"transaction-amount"}>Transaction</InputLabel>
-                                    <FilledInput
-                                        id={"transaction-amount"}
-                                        value={amount.toString()}
-                                        onChange={handleAmountChange}
-                                        startAdornment={<InputAdornment position="start">$</InputAdornment>}
-                                        type={"number"}
-                                        fullWidth
-                                    />
-                                </FormControl>
-                            </Grid2>
-                            <Grid2>
-                                <FormControl fullWidth variant={"filled"}>
-                                    <InputLabel htmlFor={"fee-amount"}>Fee</InputLabel>
-                                    <FilledInput
-                                        id={"fee-amount"}
-                                        value={fee.toFixed(3)}
-                                        startAdornment={<InputAdornment position="start">$</InputAdornment>}
-                                        inputProps={{
-                                            readOnly: true
-                                        }}
-                                        type={"number"}
-                                        fullWidth
-                                    />
-                                </FormControl>
-                            </Grid2>
-                            <Grid2>
-                                <FormControl fullWidth variant={"filled"}>
-                                    <InputLabel htmlFor={"result-amount"}>Total</InputLabel>
-                                    <FilledInput
-                                        id={"result-amount"}
-                                        value={result.toFixed(3)}
-                                        startAdornment={<InputAdornment position="start">$</InputAdornment>}
-                                        inputProps={{
-                                            readOnly: true
-                                        }}
-                                        type={"number"}
-                                        fullWidth
-                                    />
-                                </FormControl>
-                            </Grid2>
-                            {/* Bottom section */}
-                            <Grid2 container>
-                                <Grid2 container spacing={1} direction={"column"}>
-                                    <Grid2>
-                                        <Divider />
-                                    </Grid2>
-                                    <Grid2 container>
-                                        <Grid2 container spacing={3} alignItems={"center"} justifyContent={"space-between"}>
-                                            <Grid2>
-                                                <Typography variant={"overline"}>
-                                                    Using <strong>5.4% + 0.30 Fee</strong>
-                                                </Typography>
-                                            </Grid2>
-                                            <Grid2>
-                                                <Button
-                                                    variant={"contained"}
-                                                    LinkComponent={"a"}
-                                                    href={"https://github.com/JG0328/paypal-fees-calculator"}
-                                                    target={"_blank"}
-                                                >
-                                                    <GitHubIcon />
-                                                </Button>
-                                            </Grid2>
+                <Paper elevation={3} sx={{ width: "100%", p: 4, m: 4 }}>
+                    <Grid2 container spacing={3} direction={"column"}>
+                        {/* Calculator */}
+                        <Grid2>
+                            <FormControl fullWidth variant={"filled"}>
+                                <InputLabel htmlFor={"transaction-amount"}>Transaction</InputLabel>
+                                <FilledInput
+                                    id={"transaction-amount"}
+                                    value={amount.toString()}
+                                    onChange={handleAmountChange}
+                                    startAdornment={<InputAdornment position="start">$</InputAdornment>}
+                                    type={"number"}
+                                    fullWidth
+                                />
+                            </FormControl>
+                        </Grid2>
+                        <Grid2>
+                            <FormControl fullWidth variant={"filled"}>
+                                <InputLabel htmlFor={"fee-amount"}>Fee</InputLabel>
+                                <FilledInput
+                                    id={"fee-amount"}
+                                    value={fee.toFixed(3)}
+                                    startAdornment={<InputAdornment position="start">$</InputAdornment>}
+                                    inputProps={{
+                                        readOnly: true
+                                    }}
+                                    type={"number"}
+                                    fullWidth
+                                />
+                            </FormControl>
+                        </Grid2>
+                        <Grid2>
+                            <FormControl fullWidth variant={"filled"}>
+                                <InputLabel htmlFor={"result-amount"}>Total</InputLabel>
+                                <FilledInput
+                                    id={"result-amount"}
+                                    value={result.toFixed(3)}
+                                    startAdornment={<InputAdornment position="start">$</InputAdornment>}
+                                    inputProps={{
+                                        readOnly: true
+                                    }}
+                                    type={"number"}
+                                    fullWidth
+                                />
+                            </FormControl>
+                        </Grid2>
+                        {/* Bottom section */}
+                        <Grid2 container>
+                            <Grid2 container spacing={1} direction={"column"} size={12}>
+                                <Grid2>
+                                    <Divider />
+                                </Grid2>
+                                <Grid2 container>
+                                    <Grid2 container spacing={3} alignItems={"center"} justifyContent={"space-between"} size={12}>
+                                        <Grid2>
+                                            <Typography variant={"overline"}>
+                                                Using <strong>5.4% + 0.30 Fee</strong>
+                                            </Typography>
+                                        </Grid2>
+                                        <Grid2>
+                                            <Button
+                                                variant={"contained"}
+                                                LinkComponent={"a"}
+                                                href={"https://github.com/JG0328/paypal-fees-calculator"}
+                                                target={"_blank"}
+                                            >
+                                                <GitHubIcon />
+                                            </Button>
                                         </Grid2>
                                     </Grid2>
                                 </Grid2>
                             </Grid2>
                         </Grid2>
-                    </Paper>
-                </Stack>
+                    </Grid2>
+                </Paper>
             </Container>
             <Footer />
         </>
